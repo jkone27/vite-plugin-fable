80df3a7f7dabce1e3ff02c8034beafa5cf09e58a
gparmigiani <gparmigiani@travix.com>
2025-05-22 14:07:37 +0200

    add publish workflow

--- changelog-updater.js
+++ changelog-updater.js
@@ -8,7 +8,11 @@ const version = await $`bunx changelog --latest-release`.text();
 const packageVersion = await $`npm info vite-plugin-fable version`.text();
 
 if (version === packageVersion) {
-  process.exit(0);
+  console.log("version is already up to date, do not publish");
+  process.exit(1);
 }
 
 await $`npm version ${version.trim()}`;
+
+console.log(`Updated package.json version to ${version.trim()}`);
+console.log(`Run 'git push origin main --tags' after publishing to push the new version tag.`);
